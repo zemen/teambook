82455937e0ff62a7586c79feadfacebf93165491
Konstantin Semenov <zemen17@gmail.com>
2015-10-25 01:35:44 +0300

    templates

---
+++ template.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define sz(x) ((int) (x).size())
+#define forn(i, n) for (int i = 0; i < int(n); ++i)
+#define forab(i, a, b) for (int i = int(a); i < int(b); ++i)
+
+typedef long long ll;
+typedef long long ld;
+
+const int INF = int(1e9) + 100;
+const ll INFL = ll(2e18) + 100;
+
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define long long long
+
+int main()
+{
+#ifdef LOCAL
+    assert(freopen("", "r", stdin));
+#endif
+
+}
---
+++ vimrc
@@ -0,0 +1,19 @@
+syntax on
+set ai
+set si
+set nu
+set tabstop=4
+set shiftwidth=4
+cinwords+=forn,forab
+
+imap jj <esc>
+map <c-j> 5j
+map <c-k> 5k
+map gc ^i//<esc>
+map gu ^xx
+
+let $CPPFLAGS='-O2 -DLOCAL -Wall -Wextra -Wno-unused-result -std=c++11'
+map <f9> :w<cr>:make %:r<cr>
+map <f5> :!./%<<cr>
+
+imap {<CR> {<CR>}<Up><Esc>A<CR>
