2f472cfbced78260d291e57e54039396f267cd14
Konstantin Semenov <zemen17@gmail.com>
2016-03-26 18:48:29 +0300

    ordered set from STL

---
+++ algo/structures/ordered_set.cpp
@@ -0,0 +1,27 @@
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+typedef __gnu_pbds::tree<int, __gnu_pbds::null_type, std::less<int>,
+                __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update> oset;
+
+#include <iostream>
+
+int main() {
+    oset X;
+    X.insert(1);
+    X.insert(2);
+    X.insert(4);
+    X.insert(8);
+    X.insert(16);
+
+    std::cout << *X.find_by_order(1) << std::endl; // 2
+    std::cout << *X.find_by_order(2) << std::endl; // 4
+    std::cout << *X.find_by_order(4) << std::endl; // 16
+    std::cout << std::boolalpha << (end(X)==X.find_by_order(6)) << std::endl; // true
+
+    std::cout << X.order_of_key(-5) << std::endl;  // 0
+    std::cout << X.order_of_key(1) << std::endl;   // 0
+    std::cout << X.order_of_key(3) << std::endl;   // 2
+    std::cout << X.order_of_key(4) << std::endl;   // 2
+    std::cout << X.order_of_key(400) << std::endl; // 5
+}
