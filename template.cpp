2d0613f8e608c87105892060c23e3ea8b098333a
Konstantin Semenov <zemen17@gmail.com>
2016-03-27 00:21:36 +0300

    small fixes

--- template.cpp
+++ template.cpp
@@ -1,23 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define sz(x) ((int) (x).size())
 #define forn(i, n) for (int i = 0; i < int(n); ++i)
 #define forab(i, a, b) for (int i = int(a); i < int(b); ++i)
+#define sz(x) ((int) (x).size())
 
 typedef long long ll;
+typedef long long i64;
 typedef long double ld;
+typedef pair<int, int> pii;
 
-const int INF = int(1e9) + 100;
-const ll INFL = ll(2e18) + 100;
+const int inf = int(1e9) + 100;
+const ll infl = ll(2e18) + 100;
 
 #define pb push_back
 #define eb emplace_back
 #define mp make_pair
-#define long long long
 
-int main()
-{
+int main() {
 #ifdef LOCAL
     assert(freopen("", "r", stdin));
 #endif
